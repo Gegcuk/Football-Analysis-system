bfd767b64fad58fcb976fda3687cebe362dcd8f3
Aleksei Lazunin <gegcuk@gmail.com>
2024-05-28 13:14:25 +0100

    Formatted

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from utils import read_video, save_video
 from trackers import Tracker
 
-def main():
-    video_frames = read_video('input_videos/08fd33_4.mp4')
 
-    tracker = Tracker('models/best.pt')
-    tracks = tracker.get_object_tracks(video_frames, read_from_stub=True, stub_path='stubs/track_stubs.pkl')
+def main():
+    video_frames = read_video("input_videos/08fd33_4.mp4")
+
+    tracker = Tracker("models/best.pt")
+    tracks = tracker.get_object_tracks(
+        video_frames, read_from_stub=True, stub_path="stubs/track_stubs.pkl"
+    )
 
     output_video_frames = tracker.draw_annotations(video_frames, tracks)
 
-    save_video(output_video_frames, 'output_videos/output_video.avi')
+    save_video(output_video_frames, "output_videos/output_video.avi")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- utils/video_utils.py
+++ utils/video_utils.py
@@ -1,8 +1,9 @@
 import cv2
 
+
 def read_video(video_path):
     cap = cv2.VideoCapture(video_path)
-    frames= []
+    frames = []
     while True:
         ret, frame = cap.read()
         if not ret:
@@ -10,9 +11,15 @@ def read_video(video_path):
         frames.append(frame)
     return frames
 
+
 def save_video(output_video_frames, output_video_path):
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    out = cv2.VideoWriter(output_video_path, fourcc, 24, (output_video_frames[0].shape[1], output_video_frames[0].shape[0]))
+    fourcc = cv2.VideoWriter_fourcc(*"XVID")
+    out = cv2.VideoWriter(
+        output_video_path,
+        fourcc,
+        24,
+        (output_video_frames[0].shape[1], output_video_frames[0].shape[0]),
+    )
     for frame in output_video_frames:
         out.write(frame)
     out.release()
